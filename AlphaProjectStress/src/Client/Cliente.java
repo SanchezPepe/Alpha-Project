01e8efe70ebb2bd7d087448e83a6f98a503f205c
FabianOrduna <forduafe@itam.mx>
2020-03-23 12:53:17 -0600

    resolviendo conflictos

--- AlphaProjectStress/src/Client/Cliente.java
+++ AlphaProjectStress/src/Client/Cliente.java
@@ -39,12 +39,8 @@ public class Cliente extends Thread {
             //System.out.println();
             String ruta = "C:\\Users\\fabia\\Desktop\\SD\\proyecto-22032020\\Alpha-Project\\AlphaProjectStress\\src\\Stress\\resultados_registro2.txt";
             Clients.concatInFile(ruta, "J"+this.id+":"+tot+"\n");
-<<<<<<< HEAD
-            /*
-=======
-            
-            
->>>>>>> b615a37261e610cf2fc785b5b7751600cabf83f0
+
+
             MulticastClient mc = new MulticastClient();
             Player p = new Player(name, id);
             p.setMessage("");
@@ -58,11 +54,11 @@ public class Cliente extends Thread {
                 p.setMessage("Request no: " + i + " from: " + p.getNAME());
                 t1 = System.currentTimeMillis();
                 tcpClient.enviaJugador(p, i + 1);
-<<<<<<< HEAD
-            }*/
+
+            }
             
         }catch (ClassNotFoundException ex) {
-=======
+
                 t2 = System.currentTimeMillis();
                 sum += t2;
                 sum2 += t2 * t2;
@@ -73,7 +69,6 @@ public class Cliente extends Thread {
             System.out.println(Integer.toString(requests) + '\t' + Double.toString(avg) + '\t' + Double.toString(std));
             
         } catch (ClassNotFoundException ex) {
->>>>>>> b615a37261e610cf2fc785b5b7751600cabf83f0
             Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
         }
         // TODO Auto-generated catch block
