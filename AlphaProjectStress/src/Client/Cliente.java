b615a37261e610cf2fc785b5b7751600cabf83f0
sanchezpepe <sanchezpepe97@gmail.com>
2020-03-23 00:04:11 -0600

    estresamiento con requests

--- AlphaProjectStress/src/Client/Cliente.java
+++ AlphaProjectStress/src/Client/Cliente.java
@@ -40,14 +40,28 @@ public class Cliente extends Thread {
             String ruta = "ruta.txt";
             Clients.concatInFile(ruta, "J"+this.id+":"+tot+"\n");
             
+            
             MulticastClient mc = new MulticastClient();
             Player p = new Player(name, id);
             p.setMessage("");
+            
+            // Estresamiento con requests
+            long sum = 0;
+            long sum2 = 0;
+            long t1, t2;
             for (int i = 0; i < this.requests; i++) {
                 Object obj = mc.receiveUDP();
                 p.setMessage("Request no: " + i + " from: " + p.getNAME());
+                t1 = System.currentTimeMillis();
                 tcpClient.enviaJugador(p, i + 1);
+                t2 = System.currentTimeMillis();
+                sum += t2;
+                sum2 += t2 * t2;
             }
+            
+            double avg = ((double) sum) / requests;
+            double std = Math.sqrt(((double) sum2)/ (requests - avg * avg));
+            System.out.println(Integer.toString(requests) + '\t' + Double.toString(avg) + '\t' + Double.toString(std));
             
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
